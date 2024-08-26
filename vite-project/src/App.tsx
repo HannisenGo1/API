d8d3c3c3356438cf098e7e76fde89742cd37061e
HannisGo1 <Hannakarlsson69@gmail.com>
2024-08-26 14:00:28 +0200

    borttagna react api

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,20 +1,30 @@
-// qimport { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import { getPirates } from './data/getPirates'
+import { useState } from 'react'
+import { ApiData } from './data/getPirates';
 
 function App() {
-	// const [characters, setCharacters] = useState<null | object>(null)
+	const [characters, setCharacters] = useState<ApiData | null>(null);
+	const [loadingData, setLoadingData] = useState<boolean>(false);
+	const [errorData, setErrorData] = useState<string | null> (null);  
 
 	const handleGet = async () => {
+		setLoadingData(true)
+		setErrorData(null)
 		try {
 			const data = await getPirates()
 			console.log('Data from API:', data)
+			loadingData
+			setCharacters(data)
 			// TODO: visa datan i appen
 		}
 		catch(error) {
 			const e: Error = error as Error
 			console.log('API failed with error: ', e.message)
-			// TODO: visa meddelande för användaren
+			setErrorData('failed to fetch data')
+		} finally{
+		setLoadingData(false)	
 		}
 	}
 	
@@ -23,11 +33,13 @@ function App() {
 			<header>
 				<h1> Fullstack Pirates </h1>
 			</header>
-			<main>
-				<button onClick={handleGet}> Arr mateys </button>
-			</main>
-		</div>
-	)
+			          <main>
+                <button onClick={handleGet}>Arr mateys</button>
+              
+            </main>
+        </div>
+    );
+	
 }
 
 export default App
--- vite-project/src/data/getPirates.ts
+++ vite-project/src/data/getPirates.ts
@@ -1,14 +1,31 @@
+export interface Character {
+    id: number;
+    name: string;
+    films: string[];
+    videoGames: string[];
+    sourceUrl: string;
+}
+
 export interface ApiData {
-	// TODO: ta reda på vad som kommer från API:et
-	// och skriv färdigt interfacet
+    characters: Character[];
 }
 
 async function getPirates(): Promise<ApiData> {
+
+
+
 	const response: Response = await fetch('https://api.disneyapi.dev/character?films=Pirates of the Caribbean')
-	const data: ApiData = await response.json()
+ if (!response.ok) {
+        throw new Error('Network response was not okej!!!');
+    }
+    
+    const data: ApiData = await response.json();
+    return data;
+}
+	
 	// Obs! Vi är inte garanterade att få "ApiData"
 	// på onsdag kommer vi att se hur vi kan validera datan
-	return data
-}
+	
+
 
 export { getPirates }
